5408bf88c8e91cde0ced6c437de1447bfa83002f
3lbsofSalt <max.ramsdell@protonmail.com>
2023-09-22 09:17:34 -0600

    Now renders a different picture based on a value in the function

--- main.go
+++ main.go
@@ -12,6 +12,19 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
   tmpl.Execute(w, nil);
 }
 
+func handleIsWatered(w http.ResponseWriter, r *http.Request) {
+  watered := false;
+  // Do I need to initialize tmpl here?
+  var tmpl *template.Template
+  if watered {
+    tmpl = template.Must(template.ParseFiles("watered.html"));
+  } else {
+    tmpl = template.Must(template.ParseFiles("dry.html"));
+  }
+
+  tmpl.Execute(w, nil);
+}
+
 
 func main() {
   fmt.Println("listening...");
@@ -21,6 +34,7 @@ func main() {
   fs := http.FileServer(http.Dir("static"));
   mux.Handle("/static/", http.StripPrefix("/static/", fs));
   mux.HandleFunc("/", handleIndex);
+  mux.HandleFunc("/is-watered", handleIsWatered)
 
   log.Fatal(http.ListenAndServe(":8000", mux));
 }
