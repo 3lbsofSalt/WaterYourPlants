e9544b26fd444cd414d61b4929896630fca1ff53
3lbsofSalt <max.ramsdell@protonmail.com>
2023-09-27 10:39:12 -0600

    Finished the project!

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WaterYourPlants
-An HTMX + GO application that tells you how watered your plants are
+An HTMX + GO application that tells you how watered your plants are  
+I'm using this to learn go and try out HTMX.
+
+### Running
+To run it, navigate to the directory and in your terminal run the command: 
+```go run main.go```
--- main.go
+++ main.go
@@ -5,36 +5,49 @@ import (
 	"log"
 	"net/http"
   "html/template"
+  "time"
 )
+
+var lastWatered time.Time;
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
   tmpl := template.Must(template.ParseFiles("index.html"));
   tmpl.Execute(w, nil);
 }
 
-func handleIsWatered(w http.ResponseWriter, r *http.Request) {
-  watered := false;
-  // Do I need to initialize tmpl here?
-  var tmpl *template.Template
-  if watered {
-    tmpl = template.Must(template.ParseFiles("watered.html"));
-  } else {
-    tmpl = template.Must(template.ParseFiles("dry.html"));
+func isWatered(w http.ResponseWriter, r *http.Request) {
+  if r.Method == "POST" {
+    lastWatered = time.Now();
   }
 
-  tmpl.Execute(w, nil);
+  water_next := lastWatered.Add(time.Hour * 24);
+  watered := !time.Now().After(water_next);
+
+  var tmpl *template.Template;
+  if watered {
+    tmpl = template.Must(template.ParseFiles("watered.html"));
+    data := water_next.String();
+    tmpl.Execute(w, data);
+  } else {
+    tmpl = template.Must(template.ParseFiles("dry.html"));
+    tmpl.Execute(w, nil);
+  }
+
 }
 
 
 func main() {
   fmt.Println("listening...");
 
+  lastWatered = time.Now();
   mux := http.NewServeMux();
 
   fs := http.FileServer(http.Dir("static"));
+
   mux.Handle("/static/", http.StripPrefix("/static/", fs));
+
   mux.HandleFunc("/", handleIndex);
-  mux.HandleFunc("/is-watered", handleIsWatered)
+  mux.HandleFunc("/is-watered", isWatered)
 
   log.Fatal(http.ListenAndServe(":8000", mux));
 }
